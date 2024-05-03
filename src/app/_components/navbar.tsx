e34b3ae2e338f9a18365df16b8c7cb22521cce93
troypoulter <troypoulter@gmail.com>
2024-05-04 09:35:57 +1000

    fix linting and formatting

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,20 +1,19 @@
-import { Logo } from './logo';
-import { MobileSidebar } from './mobile-sidebar';
-import { SidebarRoutes } from './sidebar-routes';
+import { Logo } from "./logo";
+import { MobileSidebar } from "./mobile-sidebar";
+import { SidebarRoutes } from "./sidebar-routes";
 
 export const Navbar = () => {
-    return (
-        <div className="h-full flex bg-white border-b shadow-sm">
-            <div className="container relative flex p-4 gap-x-4 h-full items-center">
-                <MobileSidebar />
-                <div className="absolute left-1/2 transform -translate-x-1/2 md:static md:transform-none">
-                    <Logo />
-                </div>
-                <div className='hidden md:flex gap-x-4 h-full flex-col inset-y-0'>
-                    <SidebarRoutes />
-                </div>
-            </div>
-
-        </div>
-    )
-}
+	return (
+		<div className="flex h-full border-b bg-white shadow-sm">
+			<div className="container relative flex h-full items-center gap-x-4 p-4">
+				<MobileSidebar />
+				<div className="absolute left-1/2 -translate-x-1/2 transform md:static md:transform-none">
+					<Logo />
+				</div>
+				<div className="inset-y-0 hidden h-full flex-col gap-x-4 md:flex">
+					<SidebarRoutes />
+				</div>
+			</div>
+		</div>
+	);
+};
