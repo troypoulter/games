27c2944bff17a3dd098ac89d1b653f7deacbd169
troypoulter <troypoulter@gmail.com>
2024-05-03 22:32:30 +1000

    improving eslint and prettier setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+};
 
 export default nextConfig;
