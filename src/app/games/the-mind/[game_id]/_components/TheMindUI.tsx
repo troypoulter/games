ea7a82bbcc89b7052501c321a22ea196c0568050
DAN-PC\Daniel <dannash918@gmail.com>
2024-05-13 07:35:41 +1000

    mind first draft

--- src/app/games/the-mind/[game_id]/_components/TheMindUI.tsx
+++ src/app/games/the-mind/[game_id]/_components/TheMindUI.tsx
@@ -1,26 +1,152 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 "use client";
 
+import { usePartySocket } from "partysocket/react";
 import { useEffect, useState } from "react";
 
+import { Button } from "@/components/ui/button";
 import Select from "@/components/ui/Select";
+import { PARTYKIT_HOST } from "@/lib/env";
+
+const DealButton = ({ cardsToDeal, onClick }: any) => {
+	return (
+		<Button
+			className="bg-green-500 px-8 hover:bg-green-500/90"
+			onClick={() => onClick(cardsToDeal)}
+		>
+			<div>
+				<div>DEAL CARDS</div>
+			</div>
+		</Button>
+	);
+};
+
+const Keyboard = ({ cards, onClick, gameState }: any) => {
+	return (
+		<div>
+			<div className="row flex justify-center">
+				{cards.map((number: string) => (
+					<Button
+						className="m-2 px-4 py-6"
+						key={number}
+						disabled={gameState != "play"}
+						onClick={() => onClick(number)}
+					>
+						<div>{number}</div>
+					</Button>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default function TheMindGame({ gameId }: { gameId: string }) {
 	const room = gameId;
 	const playerName = localStorage.getItem("userName") || "Fred";
-	const cards = ["1", "2", "3", "4", "5", "6", "7"];
+	const cardOptions = ["1", "2", "3", "4", "5", "6", "7"];
+
+	const [numbers, setNumbers] = useState<string[]>([]);
 	const [cardsToDeal, setCardsToDeal] = useState<string>("2");
-	console.log("Cards: " + cardsToDeal);
+	const [totalCards, setTotalCards] = useState();
+	const [text, setText] = useState<string[]>([]);
+	const [playedCards, setPlayedCards] = useState(0);
+	const [cards, setCards] = useState<string[]>([]);
+	const [ws, setWs] = useState<WebSocket>(
+		new WebSocket(
+			`wss://kke8tbr1y5.execute-api.ap-southeast-2.amazonaws.com/test?room=${room}`,
+		),
+	);
+
+	const handleDeal = (numCards: any) => {
+		console.log(`Attempting to deal: ${numCards} cards`);
+		const jsonData = { action: "deal", room: room, numCards: cardsToDeal };
+		ws.send(JSON.stringify(jsonData));
+	};
+
+	const handleKeyPress = (number: string) => {
+		console.log(number + " pressed");
+		const jsonData = {
+			action: "playCard",
+			card: number,
+			player: playerName,
+			room: room,
+		};
+		ws.send(JSON.stringify(jsonData));
+
+		// remove from cards
+		const newCards = [...cards];
+		const index = newCards.indexOf(number);
+		if (index > -1) {
+			newCards.splice(index, 1);
+		}
+		setCards(newCards);
+	};
+
+	ws.onmessage = function (event) {
+		const response = JSON.parse(event.data);
+		if (response.handler == "deal") {
+			console.log("Getting a deal back");
+			console.log("Cards: " + response.cards);
+			console.log("Total Cards: " + response.totalCards);
+			setCards([]);
+			setCards(response.cards);
+			setTotalCards(response.totalCards);
+			setNumbers([]);
+			setText([]);
+			setPlayedCards(0);
+		}
+		if (response.handler == "playCard") {
+			const newPlayedCards = playedCards + 1;
+			setPlayedCards(newPlayedCards);
+			console.log("Message returned: playing number");
+			const number = response.card;
+			const player = response.player;
+			const newNumbers = [...numbers];
+			newNumbers.push(number);
+			setNumbers(newNumbers);
+			const newText = [...text];
+			const newLine = `${player} played: ${number}`;
+			newText.push(newLine);
+			setText(newText);
+		}
+	};
 
 	return (
 		<div>
-			Welcome to the Mind! Room: {room} Player: {playerName}
-			<Select
-				defaultValue={cardsToDeal}
-				selectItems={cards}
-				onChange={setCardsToDeal}
-			/>
+			<div className="row flex justify-evenly">
+				{room && <div className="">Room: {room}</div>}
+				{totalCards && (
+					<div className="">
+						Cards Played: {playedCards} / {totalCards}
+					</div>
+				)}
+			</div>
+			<div className="pt-6">
+				{text.map((t, idx) => (
+					<div key={idx} className="flex justify-center pt-2">
+						{t}
+					</div>
+				))}
+			</div>
+			<div className="absolute inset-x-0 bottom-16">
+				<Keyboard onClick={handleKeyPress} cards={cards} gameState="play" />
+			</div>
+			<div className="row absolute inset-x-0 bottom-0 flex justify-evenly">
+				<DealButton cardsToDeal={cardsToDeal} onClick={handleDeal} />
+				<Select
+					defaultValue={cardsToDeal}
+					selectItems={cardOptions}
+					onChange={setCardsToDeal}
+				/>
+			</div>
 		</div>
 	);
 }
--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,11 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-
 import React from "react";
-
-// interface DropDownItem {
-// 	label: string;
-// 	value: string;
-// }
 
 interface SelectProps {
 	defaultValue: string;
@@ -20,11 +14,8 @@ export default function Select({
 	onChange,
 }: SelectProps) {
 	return (
-		<div className="absolute inset-x-0 bottom-0 flex justify-center">
+		<div className="flex justify-center">
 			<form className="mx-auto max-w-sm">
-				<label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-					Select an option
-				</label>
 				<select
 					className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 					onChange={(e) => onChange(e.target.value)}
