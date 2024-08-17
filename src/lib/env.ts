037231984d61e69b4acd55569b7fe4e36816756a
Troy Poulter <troypoulter@gmail.com>
2024-08-17 21:05:43 +1000

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,4 +4,4 @@ export const PROTOCOL = PARTYKIT_HOST.startsWith("127.0.0.1")
 	? "http"
 	: "https";
 export const PARTYKIT_URL = `${PROTOCOL}://${PARTYKIT_HOST}`;
-export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY ?? '';
