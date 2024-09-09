436b701c4b160f2870ae206df423c15ec15a76d1
Daniel Nash <dannash918@gmail.com>
2024-09-10 09:32:35 +1000

    added party kit peel (#37)

--- src/app/games/speed-words/_party/speedwords.ts
+++ src/app/games/speed-words/_party/speedwords.ts
@@ -1,25 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import type * as Party from "partykit/server";
 
-import { GEMINI_API_KEY } from "@/lib/env";
-
-const API_KEY: string = GEMINI_API_KEY;
-
-const genAI: GoogleGenerativeAI = new GoogleGenerativeAI(API_KEY);
-
-const model = genAI.getGenerativeModel({
-	model: "gemini-1.5-flash",
-	// Set the `responseMimeType` to output JSON
-	generationConfig: { responseMimeType: "application/json" },
-});
+import {
+	getLetters,
+	initLetterPool,
+} from "../[game_id]/_components/letterFunctions";
 
 export default class SpeedWordsServer implements Party.Server {
 	constructor(readonly room: Party.Room) {}
 	gameExists: boolean = false;
 	gamePlayers: number = 0;
 	token: string = "";
+	letterPool: string[] = initLetterPool();
 
 	// TODO DAN FOLLOW THIS BLOG
 	// https://docs.partykit.io/guides/using-multiple-parties-per-project/#example-tracking-connections-across-rooms
@@ -27,6 +20,14 @@ export default class SpeedWordsServer implements Party.Server {
 	// eslint-disable-next-line @typescript-eslint/require-await
 	async onStart() {
 		console.log("Partykit server starting up");
+	}
+
+	onConnect(connection: Party.Connection) {
+		console.log("Got to onConnect");
+		// when a new client connects, send them letters
+		const newLetters = getLetters(9, this.letterPool);
+		const response = { message: "peel", data: { letters: newLetters } };
+		connection.send(JSON.stringify(response));
 	}
 
 	// This Method Creates or returns the room
@@ -39,12 +40,12 @@ export default class SpeedWordsServer implements Party.Server {
 			this.gamePlayers++;
 			if (this.gameExists) {
 				console.log(
-					`Game of Music Quiz exists, returning game! There are ${this.gamePlayers} players`,
+					`Game of Speed Words exists, returning game! There are ${this.gamePlayers} players`,
 				);
 			} else {
 				this.gameExists = true;
 				console.log(
-					`Game of Music Quiz does not exist, creating game! There are ${this.gamePlayers} players`,
+					`Game of Speed Words does not exist, creating game! There are ${this.gamePlayers} players`,
 				);
 			}
 		}
@@ -58,52 +59,20 @@ export default class SpeedWordsServer implements Party.Server {
 		return new Response("Not found", { status: 404 });
 	}
 
+	// eslint-disable-next-line @typescript-eslint/require-await
 	async onMessage(request: string) {
 		const requestJson = JSON.parse(request);
 		const message = requestJson.message;
-		console.log("Got to message in Music Quiz");
-		const token: string = requestJson.data.token;
-		this.token = token;
-		if (message === "getSongs") {
-			console.log("Got the getSong message in Music Quiz");
-			await this.getSongs();
-		}
-	}
-
-	async getSongs() {
-		console.log("Getting song now!");
-		const endpoint =
-			"https://api.spotify.com/v1/search?type=track&q=artist:Yellowcard";
-		const headers = new Headers({
-			Authorization: `Bearer ${this.token}`,
-			"Content-Type": "application/json",
-		});
-
-		await fetch(endpoint, { headers })
-			.then((response) => response.json())
-			.then((data) => console.log(JSON.stringify(data)))
-			.catch((error) => console.error("Error:", error));
-	}
-
-	async getSongsFromGemini() {
-		try {
-			const prompt = `
-				List the name of 20 popular Yellowcard songs. Using this JSON schema:
-				Song = {"song": string}
-				Return a list[Song]
-			`;
-
-			const result = await model.generateContent(prompt);
-			const promptText: string = result.response.text();
-			const jsonResponse = JSON.parse(promptText);
-			console.log(jsonResponse);
-
-			// const arrayOfStrings2 = jsonResponse.map(
-			// 	(item: { thing: string }) => item.thing,
-			// );
-			// this.wordArray = arrayOfStrings2;
-		} catch (error) {
-			console.log("Error getting words: " + JSON.stringify(error));
+		console.log("Got to message in Speed Words");
+		if (message === "peel") {
+			const connections: Iterable<Party.Connection<unknown>> =
+				this.room.getConnections();
+			const connectionsArray = Array.from(connections);
+			for (const connection of connectionsArray) {
+				const newLetters = getLetters(3, this.letterPool);
+				const response = { message: "peel", data: { letters: newLetters } };
+				connection.send(JSON.stringify(response));
+			}
 		}
 	}
 }
