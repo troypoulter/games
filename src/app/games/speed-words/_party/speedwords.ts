de6f209add3bfb42a9a08315bec55d3841c7583e
Daniel Nash <dannash918@gmail.com>
2024-09-10 13:29:26 +1000

    feat: Add some fixes (#38)

--- src/app/games/speed-words/_party/speedwords.ts
+++ src/app/games/speed-words/_party/speedwords.ts
@@ -12,7 +12,7 @@ export default class SpeedWordsServer implements Party.Server {
 	gameExists: boolean = false;
 	gamePlayers: number = 0;
 	token: string = "";
-	letterPool: string[] = initLetterPool();
+	letterPool: string[] = [];
 
 	// TODO DAN FOLLOW THIS BLOG
 	// https://docs.partykit.io/guides/using-multiple-parties-per-project/#example-tracking-connections-across-rooms
@@ -22,13 +22,17 @@ export default class SpeedWordsServer implements Party.Server {
 		console.log("Partykit server starting up");
 	}
 
-	onConnect(connection: Party.Connection) {
-		console.log("Got to onConnect");
-		// when a new client connects, send them letters
-		const newLetters = getLetters(9, this.letterPool);
-		const response = { message: "peel", data: { letters: newLetters } };
-		connection.send(JSON.stringify(response));
-	}
+	// onConnect(connection: Party.Connection) {
+	// 	console.log("Got to onConnect: " + JSON.stringify(connection));
+	// 	// // when a new client connects, send them letters
+	// 	// const newLetters = getLetters(7, this.letterPool);
+	// 	// const response = {
+	// 	// 	message: "peel",
+	// 	// 	data: { letters: newLetters },
+	// 	// };
+	// 	// connection.send(JSON.stringify(response));
+	// 	// this.broadcastLettersLeft();
+	// }
 
 	// This Method Creates or returns the room
 	async onRequest(req: Party.Request) {
@@ -65,15 +69,47 @@ export default class SpeedWordsServer implements Party.Server {
 		const message = requestJson.message;
 		console.log("Got to message in Speed Words");
 		if (message === "peel") {
-			const connections: Iterable<Party.Connection<unknown>> =
-				this.room.getConnections();
-			const connectionsArray = Array.from(connections);
-			for (const connection of connectionsArray) {
-				const newLetters = getLetters(3, this.letterPool);
-				const response = { message: "peel", data: { letters: newLetters } };
-				connection.send(JSON.stringify(response));
-			}
+			this.peel();
 		}
+		if (message === "startGame") {
+			this.startGame();
+		}
+	}
+
+	startGame() {
+		this.letterPool = initLetterPool();
+		const connections: Iterable<Party.Connection<unknown>> =
+			this.room.getConnections();
+		const connectionsArray = Array.from(connections);
+		for (const connection of connectionsArray) {
+			const newLetters = getLetters(7, this.letterPool);
+			const response = { message: "peel", data: { letters: newLetters } };
+			connection.send(JSON.stringify(response));
+		}
+
+		this.broadcastLettersLeft();
+		const response = { message: "startGame" };
+		this.room.broadcast(JSON.stringify(response));
+	}
+
+	peel() {
+		const connections: Iterable<Party.Connection<unknown>> =
+			this.room.getConnections();
+		const connectionsArray = Array.from(connections);
+		for (const connection of connectionsArray) {
+			const newLetters = getLetters(3, this.letterPool);
+			const response = { message: "peel", data: { letters: newLetters } };
+			connection.send(JSON.stringify(response));
+		}
+		this.broadcastLettersLeft();
+	}
+
+	broadcastLettersLeft() {
+		const response = {
+			message: "lettersLeft",
+			data: { lettersLeft: this.letterPool.length },
+		};
+		this.room.broadcast(JSON.stringify(response));
 	}
 }
 
